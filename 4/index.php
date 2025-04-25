3128149e71b157b438df3562dfd48cbac6ceb7bf
Kukushon <d6045033@gmail.com>
2025-04-25 13:20:52 +0300

    Update index.php

--- 4/index.php
+++ 4/index.php
@@ -70,7 +70,7 @@ function validate_form($data) {
 $user = 'u68838'; 
 $password = '4996942'; 
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=u68857', $user, $password,
+    $pdo = new PDO('mysql:host=localhost;dbname=u68838', $user, $password,
         [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 } catch (PDOException $e) {
     die("<p style='color:red;'>Ошибка подключения к базе данных: " . $e->getMessage() . "</p>");
